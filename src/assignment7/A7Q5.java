46a0aae262fedbaca73147387c19fabd41018f74
ratid6445 <ratid6445@HRH-IC0027322.wrdsb.ca>
2017-05-16 09:48:40 -0400

    Assignment7

--- src/assignment7/A7Q5.java
+++ src/assignment7/A7Q5.java
@@ -4,6 +4,8 @@
  */
 package assignment7;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ratid6445
@@ -14,15 +16,22 @@ public class A7Q5 {
      * @param args the command line arguments
      */
     public static int chaotic(int randNum) {
-
+        // form the calculation to determine random pattern
         randNum = (int) (Math.random() * (5 - 1 + 1)) + 1;
-
+        return randNum;
+        
     }
 
     public static void main(String[] args) {
         //Scanner ipnut to get user input
-        Scanner input = new Scanner 
-    
+        Scanner input = new Scanner(System.in); 
+        // ask user to input a random number
+        System.out.println("Please enter a random number below: ");
+        int randNum = input.nextInt();
+        
+        //output answer
+        double ans = chaotic(randNum);
+        System.out.println("Random Pattern: " + ans);
     
     }
 }
