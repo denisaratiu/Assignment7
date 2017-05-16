c98dda068bda9d3b51b929c5c07fe719720eae51
ratid6445 <ratid6445@HRH-IC0027322.wrdsb.ca>
2017-05-16 10:04:04 -0400

    Assignment7

--- src/assignment7/A7Q5.java
+++ src/assignment7/A7Q5.java
@@ -15,24 +15,30 @@ public class A7Q5 {
     /**
      * @param args the command line arguments
      */
-    public static int chaotic(int randNum) {
-        // form the calculation to determine random pattern
-        randNum = (int) (Math.random() * (5 - 1 + 1)) + 1;
-        randNum =  '*';
-        return randNum;
-        
+    public static void chaotic(int numberLines) {
+        // print out asterisks throughout the number of lines determined by user
+        for (int i = 0; i < numberLines; i++) {
+            //use the formula to find the pattern of asterisks at each line
+            int numAsterisks = (int) (Math.random() * (5 - 1 + 1)) + 1;
+            for (int j = 0; j < numAsterisks; j++) {
+                System.out.print("*");
+            }
+            // print out the astericsks at seperate lines
+            System.out.println("");
+        }
     }
 
     public static void main(String[] args) {
         //Scanner ipnut to get user input
-        Scanner input = new Scanner(System.in); 
-        // ask user to input a random number
+        Scanner input = new Scanner(System.in);
+        // ask user to input a random number to determine the amount of lines they want
         System.out.println("Please enter a random number below: ");
-        int randNum = input.nextInt();
-        
+        int numberLines = input.nextInt();
+
         //output answer
-        double ans = chaotic(randNum);
-        System.out.println("Random Pattern: " + ans);
-    
+        System.out.println("Random pattern: ");
+        chaotic(numberLines);
+
+
     }
 }
