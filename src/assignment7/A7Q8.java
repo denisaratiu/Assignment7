1d8b8e41314c5a795946aeb04a5ceedddc0ffb17
ratid6445 <ratid6445@HRH-IC0027322.wrdsb.ca>
2017-05-17 09:37:03 -0400

    Assignment7

---
+++ src/assignment7/A7Q8.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment7;
+
+/**
+ *
+ * @author ratid6445
+ */
+public class A7Q8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static String season(int month,int day){
+        //12/16 and 3/15, you should return "Winter". 
+        //between 3/16 and 6/15, you should return "Spring"
+        //6/16 and 9/15, you should return"Summer"
+        //9/16 and 12/15, you should return "Fall".
+
+    }
+    public static void main(String[] args){
+        
+    }
+    }
