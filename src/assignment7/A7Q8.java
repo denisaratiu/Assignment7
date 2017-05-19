11ba5dc7e20d68a1364e011f9a1424f986826dec
ratid6445 <ratid6445@HRH-IC0027322.wrdsb.ca>
2017-05-19 09:53:02 -0400

    Assignment7

--- src/assignment7/A7Q8.java
+++ src/assignment7/A7Q8.java
@@ -4,6 +4,8 @@
  */
 package assignment7;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ratid6445
@@ -13,14 +15,41 @@ public class A7Q8 {
     /**
      * @param args the command line arguments
      */
-    public static String season(int month,int day){
-        //12/16 and 3/15, you should return "Winter". 
-        //between 3/16 and 6/15, you should return "Spring"
-        //6/16 and 9/15, you should return"Summer"
-        //9/16 and 12/15, you should return "Fall".
+    public static void season(int month, int day) {
+        // fall season
+        if (month >= 9 && month <= 12 && day == 16 || day == 15) {
+            String realSeason = "fall";
+            System.out.println(realSeason);
 
+            // Spring
+        } else if (month >= 4 && month <= 6 && day == 16 || day == 15) {
+            String realSeason = "Spring";
+            System.out.println(realSeason);
+
+            // Winter
+        } else if (month >= 12 && month <= 3 && day == 16 || day == 15) {
+            String realSeason = "Winter";
+            System.out.println(realSeason);
+
+            // Summer
+        } else {
+            String realSeason = "Summer";
+            System.out.println(realSeason);
+        }
     }
-    public static void main(String[] args){
-        
+
+    public static void main(String[] args) {
+        // scanner input to get user input
+        Scanner input = new Scanner(System.in);
+        //ask user to input a the month and date
+        System.out.println("Please enter the month, as an integer, below: ");
+        int month = input.nextInt();
+        System.out.println("Please enter the day, as an integer, below: ");
+        int day = input.nextInt();
+        //output the season
+        System.out.println("The season for that date is: ");
+        String answer = season(month && day);
+
+        System.out.println(answer);
     }
-    }
+}
