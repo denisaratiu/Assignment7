7f494f809e43909b33b629cbb7176d22dd3ce9d3
ratid6445 <ratid6445@HRH-IC0027322.wrdsb.ca>
2017-05-19 09:32:10 -0400

    Assignment7

--- src/assignment7/A7Q7.java
+++ src/assignment7/A7Q7.java
@@ -15,21 +15,30 @@ public class A7Q7 {
     /**
      * @param args the command line arguments
      */
-    public static void firstDigit(double[] number) {
-        //find the first digit in the number
-        double first = number[0];
-        System.out.println(first);
+    public static int firstDigit(int number) {
+        //negative number turn to positive
+        if (number < 0) {
+            number = number * -1;
         }
-
+        
+        //find the first digit in the number
+        while(number >= 10){
+             number = number / 10;
+        }
+        return number;
+    }
+    
     public static void main(String[] args) {
         // scanner input to get user input
         Scanner input = new Scanner(System.in);
         //ask user to input a number to find the first digit
         System.out.println("Please enter a number below: ");
-        double[] number = input.nextDouble();
+        int number = input.nextInt();
 
         //output the first digit
         System.out.println("The first digit in that number is: ");
-        firstDigit(number);
+        int answer = firstDigit(number);
+        
+        System.out.println(answer);
     }
 }
