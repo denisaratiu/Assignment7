2c51ebe0216ac6ef2aa19938443b691bbdeed9bd
ratid6445 <ratid6445@HRH-IC0027322.wrdsb.ca>
2017-05-23 09:16:19 -0400

    Assignment7

--- src/assignment7/A7Q8.java
+++ src/assignment7/A7Q8.java
@@ -46,7 +46,7 @@ public class A7Q8 {
         int month = input.nextInt();
         System.out.println("Please enter the day, as an integer, below: ");
         int day = input.nextInt();
-        
+
         //output the season
         System.out.println("The season for that date is: ");
         String answer = season(month && day);
